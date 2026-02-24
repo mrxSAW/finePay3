f99ca30d0b2cececd8f0898043107d4882220623
mrxSAW <arkkass2018@gmail.com>
2026-02-24 09:48:40 +0000

    vp

---
+++ finePay p3/src/main/java/org/example/DBConnection.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+
+
+
+    public static Connection createConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/finepay?useSSL=false",
+                    "root",
+                    "123456789"
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Erreur connexion DB: " + e.getMessage(), e);
+        }
+
+    }
+
+}
---
+++ finePay p3/src/test/java/org/example/exportFileTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class exportFileTest {
+
+    @Test
+    void exporterFacturesPrestataireTesterSommeMonton() {
+    }
+
+    @Test
+    void exporterFacturImpier() {
+
+    }
+
+    @Test
+    void generateInvoice() {
+    }
+
+    @Test
+    void genererRapportMensuel() {
+    }
+}
